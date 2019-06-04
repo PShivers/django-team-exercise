b78af99105e3308466afd7f2e77813e8bfcf2cf6
Paul Shivers <paulsshivers@gmail.com>
2019-06-04 13:26:23 -0400

    created Posts component

---
+++ client/src/components/Posts.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Posts extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="posts">
+        <div />
+      </div>
+    );
+  }
+}
+
+export default Posts;
