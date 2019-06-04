056b0ac7922956347aeb46a14ca918121a3a709b
Sarah Trapaga <trapagasarah@gmail.com>
2019-06-04 12:18:39 -0400

    installed restframework

--- redditish_django/settings.py
+++ redditish_django/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'redditish',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {    
+    'DEFAULT_PERMISSION_CLASSES': [    
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'    
+    ], 
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),    
+} 
