ce8c099c85a81908a1d0606c1dba75fe03820426
Paul Shivers <paulsshivers@gmail.com>
2019-06-04 23:24:11 -0400

    fixed issue with backend not displaying JSON data. IntegerField => IntegerField() in models.py

--- redditish/views.py
+++ redditish/views.py
@@ -1,11 +1,13 @@
-from rest_framework import viewsets 
-from .serializer import PostSerializer, CommentSerializer 
+from rest_framework import viewsets
+from .serializer import PostSerializer, CommentSerializer
 from .models import Post, Comment
-                                                         
+
+
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    
+
+
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
